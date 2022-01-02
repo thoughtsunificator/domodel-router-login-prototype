3e77df9e8617e58a73d408f10f02b4b04fb6fec2
Romain Lebesle <oss@thoughtsunificator.me>
2022-01-02 21:21:44 +0100

    fix

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import postcss from "rollup-plugin-postcss"
 import postcssImport from "postcss-import"
 import postcssEasyImport  from "postcss-easy-import"
 import { terser } from "rollup-plugin-terser"
-import copy from "rollup-plugin-copy-watch"
+import copy from "rollup-plugin-copy"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
 import serve from "rollup-plugin-serve"
 import livereload from "rollup-plugin-livereload"
@@ -14,18 +14,6 @@ import config from "@thoughtsunificator/config-env"
 const isProduction = process.env.BUILD === "production"
 const isDevelopment = !isProduction
 
-function replaceHTMLVars() {
-	fs.readFile("public/index.html", 'utf8', function (err,data) {
-		if (err) {
-			return console.log(err)
-		}
-		const result = data.replace(/%{BASE_HREF}%/g, config.basePath)
-		fs.writeFile("public/index.html", result, 'utf8', function (err) {
-			 if (err) return console.log(err)
-		})
-	})
-}
-
 export default {
 	input: "src/main.js",
 	output: {
@@ -33,19 +21,34 @@ export default {
 		format: "iife",
 		sourcemap: isDevelopment
 	},
+	watch: {
+		include: ["src/**/*"]
+	},
 	plugins: [
-		replaceHTMLVars(),
 		rollupWindowEnv({ envPath : ".env.json", confPath : "data/config.json" }),
 		postcss({
 			minimize: isProduction,
 			sourceMap: isDevelopment,
 			extract: true,
+			dependencies: ["src/**/*.css"],
+			config: {
+				watch: true
+			},
 			plugins: [postcssEasyImport(), postcssImport()]
 		}),
 		isProduction && terser(),
 		copy({
 			watch: isDevelopment ? "public" : false,
-			targets: [{ src: "public/*", dest: "dist" }],
+			targets: [{
+				src: ["public/*"],
+				dest: "dist",
+				transform: function(contents, filename) {
+					if(filename === "index.html") {
+						return contents.toString().replace(/%{BASE_HREF}%/g, config.basePath)
+					}
+					return contents
+				}
+			}],
 			flatten: false
 		}),
 		nodeResolve(),
